96b9c68d98d3ce353cf075658addbdd253e620f0
mohitsuper <animemusically6@gamil.com>
2024-10-02 15:39:18 +0530

    first

---
+++ gulabjichai/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../commnts/Navbar'
+
+export default function () {
+  return (
+    <div style={{
+    }} className='h-[100vh] w-full bg-[url("./asstes/images/bg1.png")] bg-center '>
+        <Navbar/>
+    </div>
+  )
+}
